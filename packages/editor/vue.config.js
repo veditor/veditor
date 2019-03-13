2af696811fad92e4442b7c73ea1b71973ab5c1f9
Jagger <linjie.gao@tendcloud.com>
2019-03-13 19:19:21 +0800

    fix

--- packages/editor/vue.config.js
+++ packages/editor/vue.config.js
@@ -2,6 +2,6 @@ const path = require('path')
 
 module.exports = {
   chainWebpack: config => {
-    config.resolve.alias.set('@veditor/ui', path.resolve(__dirname, '../ui/dist/vui.common.js'))
+    config.resolve.alias.set('@veditor/ui', path.resolve(__dirname, '../ui/'))
   }
 }
